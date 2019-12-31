f54c3d6fa4f42d08af69a8e3722d5ca0eda22a46
Reduichi <57396815+Reduichi@users.noreply.github.com>
2019-12-31 21:32:15 +0800

    作業未完成

--- UnityPetFollow/Assets/Scripts/Dragon.cs
+++ UnityPetFollow/Assets/Scripts/Dragon.cs
@@ -2,30 +2,29 @@
 
 public class Dragon : MonoBehaviour
 {
-    [Header("速度"), Range(0, 100)]
-    public float speed = 1.5f;
-
-    private Transform player;
+    public Transform target,drag;
 
     private void Start()
     {
-        player = GameObject.Find("野蠻人").transform;
+        target = GameObject.Find("目標").transform;
     }
-    // 在 Update 之後才執行 : 攝影機追蹤，物件追蹤
-    private void LateUpdate()
+
+    // 固定更新 : 一秒執行50次，處理物理行為
+    private void FixedUpdate()
     {
         Track();
     }
 
     /// <summary>
-    /// 恐龍追蹤效果
+    /// 跟隨玩家的目標
     /// </summary>
     private void Track()
     {
-        Vector3 posP = player.position;         // 野蠻人
-        Vector3 posC = transform.position;      // 恐龍
+        Vector3 posD = drag.position;         // 恐龍
+        Vector3 posT = target.position;       // 玩家目標
 
-        // 恐龍.座標 = 三維向量(A座標，B座標，百分比)
-        transform.position = Vector3.Lerp(posC, posP, 0.3f * Time.deltaTime * speed);
+        posT.z += 8;      
+
+        transform.position = Vector3.Lerp(posD, posT, 0.3f * Time.deltaTime );
     }
 }
